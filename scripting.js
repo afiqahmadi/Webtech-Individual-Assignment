0dac75043635f4cc5f0e3e736c7669c8b1efc393
afiqahmadi <152852598+afiqahmadi@users.noreply.github.com>
2023-12-04 17:13:23 +0800

    scripting.js

--- scripting.js
+++ scripting.js
@@ -17,66 +17,8 @@ function openfb()
 var fbbutton = document.getElementById("fbbutton");
 fbbutton.addEventListener("click" , openfb);
 
-function formsubmit(event)
-        {
-            event.preventDefault();
-
-            var name = document.getElementById("name");
-            var staffid = document.getElementById("staffid");
-            var kcdio = document.getElementById("kcdio");
-            var contact = document.getElementById("contact");
-            var email = document.getElementById("email");
-            var amount = document.getElementById("amount");
-            var monthstart = document.getElementById("monthstart");
-            var yearstart = document.getElementById("yearstart");
-            var monthend = document.getElementById("monthend");
-            var yearend = document.getElementById("yearend");
-            var kuliyyah = document.getElementById("kuliyyah");
-
-            var nameval = name.value;
-            var staffidval = staffid.value;
-            var kcdioval = kcdio.value;
-            var contactval = contact.value;
-            var emailval = email.value;
-            var amountval = amount.value;
-            var monthstartval = monthstart.value;
-            var yearstartval = yearstart.value;
-            var monthendval = monthend.value;
-            var yearendval = yearend.value
-            var kuliyyahval = kuliyyah.value;
-
-            if(kuliyyahval == "NULL")
-            {
-               <p>
-               
-                    "Name : " + nameval + "\n" +
-                    "Staff ID : " + staffidval + "\n" +
-                    "K / C / D / I / O : " + kcdioval + "\n" +
-                    "Contact NO. : " + contactval + "\n" +
-                    "e-mail : " + emailval + "\n" +
-                    "Amount : RM " + amountval + "\n" +
-                    "The above salary deduction will take effect from " + monthstartval + " " + yearstartval + " until "
-                    + monthendval + " " + yearendval
-
-                    window.location.href = ""
-               </p>
-            }
-            else
-            {
-                <p>
-                    "Name : " + nameval + "\n" +
-                    "Staff ID : " + staffidval + "\n" +
-                    "K / C / D / I / O : " + kcdioval + "\n" +
-                    "Contact NO. : " + contactval + "\n" +
-                    "e-mail : " + emailval + "\n" +
-                    "Amount : RM " + amountval + "\n" +
-                    "The above salary deduction will take effect from " + monthstartval + " " + yearstartval + " until "
-                    + monthendval + " " + yearendval + "\n" +
-                    "For kulliyyah of " + kuliyyahval
-                </p>
-            }
-            
-        }
-
-        var form = document.getElementById("reductionform").formTarget = "=_self";
-        form.addEventListener("submit" , formsubmit);
+document.getElementById("reductionform").addEventListener("submit" , function(event)
+{
+    event.preventDefault();
+    window.location.href = "http://127.0.0.1:5500/submit.html";
+});
